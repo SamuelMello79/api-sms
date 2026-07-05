2b21a307dbea2a16c45deafebf84ba3b2b3557fb
SamuelMello <mello.developer797@gmail.com>
2026-07-05 13:45:27 -0300

    [CORE] Iniciando projeto

---
+++ src/main/java/dev/mello/api_sms/ApiSmsApplication.java
@@ -0,0 +1,13 @@
+package dev.mello.api_sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mello/api_sms/ApiSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mello.api_sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
